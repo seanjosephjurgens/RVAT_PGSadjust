4ecaa2f62f04892959b77a44ae831c9cfdb15fac
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2022-11-08 21:25:16 +0100

    Update saige_step0_make_sparse_matrix_submit.sh

--- SAIGE_GENE+/saige_step0_make_sparse_matrix_submit.sh
+++ SAIGE_GENE+/saige_step0_make_sparse_matrix_submit.sh
@@ -3,9 +3,9 @@
 PROJECT="RAP_PROJECT_NAME"
 
 ~/dx/dx-toolkit/bin/dx run swiss-army-knife \
--iin="${RAP_PROJECT_NAME}:path/to/array_data_for_saige/ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01.bed" \
--iin="${RAP_PROJECT_NAME}:path/to/array_data_for_saige/ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01.bim" \
--iin="${RAP_PROJECT_NAME}:path/to/array_data_for_saige/ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01.fam" \
+-iin="${PROJECT}:path/to/array_data_for_saige/ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01.bed" \
+-iin="${PROJECT}:path/to/array_data_for_saige/ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01.bim" \
+-iin="${PROJECT}:path/to/array_data_for_saige/ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01.fam" \
 -icmd="createSparseGRM.R --plinkFile=ukbb200k_all_commonvariantsfromarray_chrall_and_rarevariantsfromWES_chr22_geno0.01 --nThreads=94  --outputPrefix=saige_sparse_matrix --numRandomMarkerforSparseKin=5000 --relatednessCutoff=0.05" \
 -iimage_file="${RAP_PROJECT_NAME}:path/to/docker/saige_1.0.9.tar.gz" \
 --name saige_step0 \
